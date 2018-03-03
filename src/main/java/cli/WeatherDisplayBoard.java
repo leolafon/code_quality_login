5e5b79e3d5309054d6093cb997be11b16fd7ac73
Jamais <johan.paasche@epitech.eu>
2018-03-03 15:33:26 +0100

    Adding WeatherDisplayBoard to display many dates forecast

---
+++ src/main/java/cli/WeatherDisplayBoard.java
@@ -0,0 +1,61 @@
+package cli;
+
+import org.json.JSONArray;
+
+public class WeatherDisplayBoard {
+    private int nbOfBlocks = 0;
+    private String cityTitle = "MetaWeather";
+    private int widthOfDisplay = 0;
+    JSONArray daysForecastArray = null;
+    WeatherBlockBoard weatherBlockArray[] = null;
+
+    public WeatherDisplayBoard(String location, JSONArray daysForecastArray)
+    {
+        this.cityTitle = location;
+        if (daysForecastArray == null)
+            return;
+        this.daysForecastArray = daysForecastArray;
+        this.nbOfBlocks = daysForecastArray.length();
+        setupBlockArrayFromData();
+    }
+
+    private void setupBlockArrayFromData()
+    {
+        if (this.daysForecastArray != null && this.nbOfBlocks > 0)
+        {
+            this.weatherBlockArray = new WeatherBlockBoard[this.nbOfBlocks];
+            for (int i = 0; i < this.daysForecastArray.length(); i++)
+            {
+                this.weatherBlockArray[i] = new WeatherBlockBoard(this.daysForecastArray.getJSONObject(i));
+
+            }
+        }
+    }
+
+    private void calculateWidthOfDisplay()
+    {
+        this.widthOfDisplay = 0;
+        for (int i = 0; i < this.daysForecastArray.length(); i++)
+        {
+            this.widthOfDisplay += this.weatherBlockArray[i].getLenghtOfData() + 2;
+        }
+    }
+
+    public void drawBorder() {
+        String border = "+";
+        for (int i = 0; i < this.widthOfDisplay; i++)
+            border += '-';
+        border += '+';
+        System.out.println(border);
+    }
+
+    public void drawCell(String cellText) {
+        String cellContent = "| ";
+        cellContent += cellText;
+        cellContent += " |";
+    }
+
+    public void displayWeather()
+    {
+    }
+}
