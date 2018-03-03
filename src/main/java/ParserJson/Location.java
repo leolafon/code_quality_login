55c123edf968f6b32790fff7011c3c68c7f7a48b
Odan <odan@Jean-Michel.Crapaud>
2018-03-03 15:36:20 +0100

    noboday care anyway

--- src/main/java/ParserJson/Location.java
+++ src/main/java/ParserJson/Location.java
@@ -1,10 +1,18 @@
-import {Pos.java};
+import Pos.java;
 
 public class Location {
-	private String: title;
-	private String: locationType;
-	private Number: woeid;
-	private Pos: pos;
+	private String title;
+	private String locationType;
+	private Number woeid;
+	private Pos pos;
+
+
+	public SetLocation(json){
+		this.setTitle(testJson.title.toString());
+		this.setTitle(testJson.location_type.toString());
+		this.setWoeid(testJson.woeid);
+		this.setPos(testJson.latt_long);
+	}
 
 	public setTitle(String title){
 		this.title = title;
@@ -23,5 +31,21 @@ public class Location {
 		String longString;
 		lattString = pos.substring(0,pos.find(','));
 		longString = pos.substring(str.fin(',')+1,pos.lenght)
+	}
+
+	public String getTitle(){
+		return this.title;
+	}
+
+	public String getLocationType(){
+		return this.locationType;
+	}
+
+	public Number getWoreid(){
+		return this.woeid;
+	}
+
+	public Pos getPos(){
+		return this.pos;
 	}
 }
--- src/main/java/ParserJson/Main.java
+++ src/main/java/ParserJson/Main.java
@@ -1,5 +1,7 @@
+import Location.java
+
 public class Main {
-public testJson = {"title":"Bordeaux","location_type":"City","woeid":580778,"latt_long":"44.836632,-0.581050"};
+	public testJson = {"title":"Bordeaux","location_type":"City","woeid":580778,"latt_long":"44.836632,-0.581050"};
 	public testLocation = new Location;
     }
 }
