1e6a3b92ddc2a13db7b63b287ef0515f04657d27
Jamais <johan.paasche@epitech.eu>
2018-03-03 16:39:42 +0100

    Adding Link between CLI & Display module

--- src/main/java/cli/Cli.java
+++ src/main/java/cli/Cli.java
@@ -1,15 +1,27 @@
 package cli;
 
 import metaweather.Api;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class Cli {
     private String  city = "";
     private int     nbDays = 0;
     private Api     weatherApi = Api.getInstance();
+    private JSONObject location;
+    private WeatherDisplayBoard displayBoard = null;
 
     Cli(String city, int nbDays) {
         this.city = city;
         this.nbDays = nbDays;
+        this.location = Api.locationSearch(this.city);
+        JSONObject consolidateWeather = Api.getWeatherByLocation(this.city);
+        if (consolidateWeather != null)
+        {
+            JSONArray Forecasts = consolidateWeather.getJSONArray("consolidated_weather");
+            this.displayBoard = new WeatherDisplayBoard(this.city, Forecasts);
+        }
+
     }
 
     public String getCity() {
